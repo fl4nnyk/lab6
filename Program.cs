26379cd7c98f2ecf0e03282d2f16de7991e3c97b
fl4nnyk <112153929+fl4nnyk@users.noreply.github.com>
2022-11-29 10:38:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace lab6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2);
+
+            foreach (int item in list.GetArray())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(MyClass<List<int>>.FactoryMethod());
+        }
+    }
+}
